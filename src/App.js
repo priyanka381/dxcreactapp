1c93a026967af292cc12fb26cb1979a3f8d62d18
priyanka381 <84623060+priyanka381@users.noreply.github.com>
2021-11-12 10:44:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import Form from './components/Form';
 import Fragments from './components/Fragments';
 import NewTable from './components/NewTable';
 
+import PureComp from './components/PureComp';
+import Parentcomp from './components/Parentcomp';
+
 
 function App() {
   return (
     <div className="App">
-      <Form/>
-      <Fragments/>
-      <NewTable/>
+      
+      <Parentcomp/>
+      
+      
+      
     </div>
   );
 }
